df9532d1f1d5141a9b8b3b4a9a8dbaa4b20b7761
Subhrotechinfo <subhro.techinfo@gmail.com>
2018-08-21 18:53:22 +0530

    Added Error handling support for async

--- currency-convert.js
+++ currency-convert.js
@@ -11,10 +11,20 @@ const axios  = require('axios');
 // };
 //async version
 const getExchangeRate = async (from , to) => {
-  const response = await axios.get('http://data.fixer.io/api/latest?access_key=0bbba1f10ae4c09b748e86c425647ee9&format=1');
-  const euro = 1 /response.data.rates[from];
-  const rate = euro * response.data.rates[to];
-  return rate;
+    try{
+      const response = await axios.get('http://data.fixer.io/api/latest?access_key=0bbba1f10ae4c09b748e86c425647ee9&format=1');
+      const euro = 1 /response.data.rates[from];
+      const rate = euro * response.data.rates[to];
+
+      if(isNaN(rate)){
+        throw new Error();
+      }
+
+      return rate;
+    }catch(e){
+      throw new Error(`\nSomething went wrong while fetching the rates for ${from} and ${to}.`);
+        //return 'Something went wrong while fetching the rates';
+    }
 };
 
 // const getCountries = (currencyCode) => {
@@ -25,8 +35,12 @@ const getExchangeRate = async (from , to) => {
 
 //async version
 const getCountries = async (currencyCode) => {
-  var response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-    return response.data.map((country) => country.name );
+  try{
+    var response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+      return response.data.map((country) => country.name );
+  }catch(e){
+    throw new Error(`\nSomething went wrong while fetching the countries using ${currencyCode}.`);
+  }
 };
 
 // const convertCurrency = (from ,  to,  amount) => {
@@ -52,10 +66,25 @@ const convertCurrency = async (from ,  to,  amount) => {
 
 convertCurrency('USD','EUR',20).then((message) => {
   console.log(message)
-}).catch((e) => {
-  console.log('Invalid country');
+}).catch((err) => {
+  console.log('Invalid country' , err.message);
 });
 
+// const add = async (a, b) => a + b + c;
+// const doWork = async () => {
+//   try{
+//     const result  = await add(8,25);
+//     return result;
+//   }catch(e){
+//     return 'Error found';
+//   }
+// };
+//
+// doWork().then((data) => {
+//   console.log(data);
+// }).catch((e) => {
+//   console.log('Something went wrong');
+// })
 // getCountries('INR').then((countries)=> {
 //   console.log(countries);
 // });
